e14d7dff4806d8203b67895137cb5c90cf9fa8e4
Xuntron <seeratsdsking@gmail.com>
2019-04-14 03:35:03 +0500

    rating

---
+++ algo.php
@@ -0,0 +1,8 @@
+<?php
+
+$positiveWords = array("good", "great", "approve", "thanks", "amazing", "calm", "classic", "classic", "cool");
+$negativeWords = array("bad", "annoy", "broken", "boring", "confused", "corrupt", "damage", "fail", "foul");
+
+$string = "piece1 piece2 piece3 piece4 piece5 piece6";
+$pieces = explode(" ", $string);
+
