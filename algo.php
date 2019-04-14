b7a808f8e6a4ab1d5c0be1ca7a4e1c2efb911e04
Xuntron <seeratsdsking@gmail.com>
2019-04-14 09:36:30 +0500

    commit

--- algo.php
+++ algo.php
@@ -1,8 +1,36 @@
 <?php
 
-$positiveWords = array("good", "great", "approve", "thanks", "amazing", "calm", "classic", "classic", "cool");
-$negativeWords = array("bad", "annoy", "broken", "boring", "confused", "corrupt", "damage", "fail", "foul");
+$positiveWords = array("good", "great", "approve", "thanks", "amazing", "calm", "love", "classic", "cool", "fabulous", "fair", "favorable", "fortunate", "fine", "impressive");
+$negativeWords = array("bad", "annoy", "broken", "boring", "confused", "corrupt", "damage", "fail", "foul", "can't", "deny", "imperfect", "negative", "not", "poor");
 
-$string = "piece1 piece2 piece3 piece4 piece5 piece6";
+$positiveFlag = 0;
+$negativeFlag = 0;
+
+$string = "I love this game it has so much detail and it's free! Usually car simulators that have this much detail would cost money. You should get this game. By far the best car game I have";
 $pieces = explode(" ", $string);
 
+$positiveSame= array_intersect($positiveWords, $pieces);
+$negativeSame = array_intersect($negativeWords, $pieces);
+
+if(count($positiveSame) > count($negativeSame))
+{
+    $positiveFlag += 10;
+}
+else
+{
+    $negativeFlag += 5;
+}
+
+if(count($positiveSame) == count($negativeSame))
+{
+    $positiveFlag += 2;
+}
+
+if(count($positiveSame) == 0 && count($negativeSame) == 0)
+{
+    $positiveFlag = 0;
+    $negativeFlag = 0;
+}
+
+echo $positiveFlag;
+echo " / ". $negativeFlag;
